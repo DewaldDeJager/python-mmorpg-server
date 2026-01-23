a82306e8974cfc9acd93a8a48966c4aee12eb7f1
Dewald de Jager <dewald.dejager@entelect.co.za>
2026-01-23 23:03:38 +0200

    Implement the queueing of packets to be sent to players

--- game/entity/character/player/player.py
+++ game/entity/character/player/player.py
@@ -4,6 +4,8 @@ from game.world import World
 from network import EntityData
 from network.connection import Connection
 from network.impl.connected import ConnectedPacket
+from game.packet_data import PacketData
+from network.modules import PacketType
 from network.packet import Packet
 
 
@@ -25,10 +27,19 @@ class Player(Character):
         self.stop_intervals()
 
     def send(self, packet: Packet) -> None:
-        self.world.network_manager.send(self.connection.instance, packet)
+        """
+        We create this function to make it easier to send
+        packets to players instead of always importing `world`
+        in other classes.
+        @param packet Packet we are sending to the player.
+        """
+        self.world.push(PacketType.Player, PacketData(packet=packet, player=self))
 
     def serialize(self, with_equipment: bool = False, with_experience: bool = False,
                   with_mana: bool = False) -> EntityData:
         data = super().serialize()
 
         return data
+
+
+PacketData.model_rebuild()
--- game/packet_data.py
+++ game/packet_data.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 from typing import List, Optional, TYPE_CHECKING
 
+from pydantic import ConfigDict
+
 if TYPE_CHECKING:
     from game.entity.character.player.player import Player
 
@@ -8,6 +10,8 @@ from network.packet import Packet
 from network.model import CamelModel
 
 class PacketData(CamelModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
     packet: Packet
     player: Optional[Player] = None
     players: Optional[List[Player]] = None
